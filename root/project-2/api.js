535c8e2d23b831c550d903d3ed2046e92c961741
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-10 12:00:39 +0800

    feat: add query selector; add query selector to async function

--- root/project-2/api.js
+++ root/project-2/api.js
@@ -1,11 +1,17 @@
 
 
 const apiKey = "e011851732e39843d44a5db598983b1b";
-const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=manila";
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=tokyo";
 
 async function checkWeather() {
     const response = await fetch(weatherApiUrl + `&appid=${apiKey}`);
     let data = await response.json();
+
+    console.log(data);
+    document.querySelector('.js-city-name').textContent = data.name;
+    document.querySelector('.js-temperature').textContent = Math.round(data.main.temp);
+    document.querySelector('.humidity').textContent = data.main.humidity + `%`;
+    document.querySelector('.wind-speed').textContent = data.wind.speed + ` km/h`;
 }
 
 checkWeather();
