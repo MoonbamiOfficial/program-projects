72b749c6ada6d6d7aa98677a47e921cd4f913df6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-10 15:03:25 +0800

    chore: remove console.log

--- root/project-2/api.js
+++ root/project-2/api.js
@@ -14,7 +14,6 @@ async function checkWeather(cityName) {
     const response = await fetch(weatherApiUrl + cityName + `&appid=${apiKey}`);
     let data = await response.json();
 
-    console.log(data);
     document.querySelector('.js-city-name').textContent = data.name;
     document.querySelector('.js-temperature').textContent = Math.round(data.main.temp);
     document.querySelector('.humidity').textContent = data.main.humidity + `%`;
