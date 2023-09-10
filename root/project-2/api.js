34fb208b522e316c3c2bcdf4575cf62e07bd2e97
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-10 12:29:13 +0800

    feat: add event listeners; add event listeners to search

--- root/project-2/api.js
+++ root/project-2/api.js
@@ -1,10 +1,19 @@
 
 
 const apiKey = "e011851732e39843d44a5db598983b1b";
-const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=tokyo";
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(weatherApiUrl + `&appid=${apiKey}`);
+const searchInput = document.querySelector('.js-input')
+    searchInput.addEventListener('keydown', (event) => handleUserInput(event));
+const searchBtn = document.querySelector('.js-search-btn')
+    .addEventListener('click', () => checkWeather(searchInput.value));
+
+function handleUserInput(event) {
+    if(event.key === 'Enter') checkWeather(searchInput.value);
+}
+
+async function checkWeather(cityName) {
+    const response = await fetch(weatherApiUrl + cityName + `&appid=${apiKey}`);
     let data = await response.json();
 
     console.log(data);
