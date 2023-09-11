3314a87244497763aa45f0842f74819a94cc4c93
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-11 21:36:24 +0800

    feat: add selectors to script

---
+++ root/v.1.4-task-app/script.js
@@ -0,0 +1,11 @@
+
+const taskInput = document.querySelector('#task-input');
+const taskList = document.querySelector('#list-container');
+const addBtn = document.querySelector('.js-add-btn');
+
+
+
+
+
+
+
