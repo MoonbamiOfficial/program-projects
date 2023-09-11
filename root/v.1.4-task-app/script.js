d6eb833bcacb1196a90c83ba4f6d4b7198147d2f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-11 22:04:15 +0800

    feat: add function; add addTask function

--- root/v.1.4-task-app/script.js
+++ root/v.1.4-task-app/script.js
@@ -1,9 +1,22 @@
 
 const taskInput = document.querySelector('#task-input');
 const taskList = document.querySelector('#list-container');
-const addBtn = document.querySelector('.js-add-btn');
+const addBtn = document.querySelector('.js-add-btn')
+    .addEventListener('click', () => addTask());
 
 
+function addTask() {
+    if(taskInput.value === '') alert("Write something first!");
+    else {
+        let list = document.createElement("li");
+        list.textContent = taskInput.value;
+        taskList.appendChild(list);
+        let deleteTask = document.createElement("img");
+        deleteTask.src = "assets/delete.png";
+        list.appendChild(deleteTask);
+    }
+    taskInput.value = '';
+}
 
 
 
