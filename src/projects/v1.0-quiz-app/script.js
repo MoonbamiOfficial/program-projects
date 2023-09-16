47c1495b3211b6cf6dcf7b24db648972000704f6
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-16 09:07:39 +0800

    feat: add questions

--- src/projects/v1.0-quiz-app/script.js
+++ src/projects/v1.0-quiz-app/script.js
@@ -1,5 +1,30 @@
 
-
-
-
-
+const questions = [
+    {
+        question: "Who is the main character in Fairy Tail?",
+        answers: [
+            {text : "Natsu", correct: false},
+            {text : "Lucy", correct: true},
+            {text : "Gray", correct: false},
+            {text : "Erza", correct: false}
+        ]
+    },
+    {
+        question: "Who is the \"beater\" in Sword Art Online?",
+        answers: [
+            {text : "Asuna", correct: false},
+            {text : "Silica", correct: false},
+            {text : "Kirito", correct: true},
+            {text : "Alice", correct: false}
+        ]
+    },
+    {
+        question: "Which Pyrokinetic generation has the ability to generate fire from themselves?",
+        answers: [
+            {text : "3rd", correct: true},
+            {text : "2nd", correct: false},
+            {text : "1st", correct: false},
+            {text : "None of the Above", correct: false}
+        ]
+    },
+];
