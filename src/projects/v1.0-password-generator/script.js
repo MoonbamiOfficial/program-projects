ec8d7fa365d9a638be0ce9ed674f3058ae761b93
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-19 21:33:47 +0800

    feat: add variables and selectors

---
+++ src/projects/v1.0-password-generator/script.js
@@ -0,0 +1,9 @@
+
+const passwordInput = document.querySelector('.js-input-pass');
+const minPasswordLength = 6;
+const maxPasswordLength = 15;
+
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '1234567890';
+const symbols = "~`!@#$%^&*()_+-=?><";
