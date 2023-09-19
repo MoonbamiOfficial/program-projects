7949b681c31529f0d1047309bcd7ebb9f5602421
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-19 21:45:55 +0800

    feat: add function; add generatePassword function

--- src/projects/v1.0-password-generator/script.js
+++ src/projects/v1.0-password-generator/script.js
@@ -1,5 +1,8 @@
 
-const passwordInput = document.querySelector('.js-input-pass');
+const passwordInput = document.querySelector('.js-input-pass')
+const generatePassBtn = document.querySelector('.js-generate-pass-btn')
+generatePassBtn.addEventListener('click', generatePassword);
+
 const minPasswordLength = 6;
 const maxPasswordLength = 15;
 
@@ -7,3 +10,18 @@ const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '1234567890';
 const symbols = "~`!@#$%^&*()_+-=?><";
+
+const allCharacters = lowerCaseLetters + numbers + upperCaseLetters + symbols;
+
+function generatePassword() {
+    let password = "";
+    password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(maxPasswordLength > password.length) {
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    }
+    passwordInput.value = password;
+}
