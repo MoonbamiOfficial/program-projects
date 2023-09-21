a0cb8c3d12a681f6386aa0e37eed2c5a8d35caf7
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-21 23:46:51 +0800

    feat: add selector and variables

---
+++ src/projects/v1.0-notes-app/script.js
@@ -0,0 +1,4 @@
+
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.js-create-notes-btn');
+let notes = document.querySelectorAll('.input');
