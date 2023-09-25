be50d807b5ca064aa48a17bdc2dd3b337c91eb00
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-25 21:59:36 +0800

    feat: add switch; add switch to validate the plural forms of year, month and day

--- src/projects/v1.0-age-calculator-app/script.js
+++ src/projects/v1.0-age-calculator-app/script.js
@@ -1,6 +1,7 @@
 
 let userSelect = document.querySelector('#date');
 userSelect.max = new Date().toISOString().split("T")[0];
+const ageDisplay = document.querySelector('.js-age-display');
 const calculateAgeBtn = document.querySelector('.js-calculate-age-btn')
     .addEventListener('click', calculateAge);
 
@@ -37,6 +38,33 @@ function calculateAge() {
         getMonth = 11;
         getYear--;
     }
+
+    switch(true) {
+        case getYear >= 2 && getMonth >= 2 && getDay >= 2: 
+            ageDisplay.textContent = `You are ${getYear} years, ${getMonth} months and ${getDay} days old.`;
+            break;
+        case getYear <= 1 && getMonth >= 2 && getDay >= 2: 
+            ageDisplay.textContent = `You are ${getYear} year, ${getMonth} months and ${getDay} days old.`;
+            break;
+        case getYear >= 2 && getMonth <= 1 && getDay >= 2: 
+            ageDisplay.textContent = `You are ${getYear} years, ${getMonth} month and ${getDay} days old.`;
+            break;
+        case getYear >= 2 && getMonth >= 2 && getDay <= 1: 
+            ageDisplay.textContent = `You are ${getYear} years, ${getMonth} months and ${getDay} day old.`;
+            break;
+        case getYear <= 1 && getMonth <= 1 && getDay >= 2: 
+            ageDisplay.textContent = `You are ${getYear} year, ${getMonth} month and ${getDay} days old.`;
+            break;
+        case getYear >= 2 && getMonth <= 1 && getDay <= 1: 
+            ageDisplay.textContent = `You are ${getYear} years, ${getMonth} month and ${getDay} day old.`;
+            break;
+        case getYear <= 1 && getMonth >= 2 && getDay <= 1: 
+            ageDisplay.textContent = `You are ${getYear} year, ${getMonth} months and ${getDay} days old.`;
+            break;
+        case getYear <= 1 && getMonth <= 1 && getDay <= 1: 
+            ageDisplay.textContent = `You are ${getYear} year, ${getMonth} month and ${getDay} day old.`;
+            break;
+    };
 }
 function getDaysInMonth(year, month) {
     return new Date(year, month, 0).getDate();
