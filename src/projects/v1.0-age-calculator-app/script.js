7a6516df1482f406d65e8b712a31f24a73f08e01
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-25 21:29:17 +0800

    feat: add new function; add new calculateAge function

--- src/projects/v1.0-age-calculator-app/script.js
+++ src/projects/v1.0-age-calculator-app/script.js
@@ -1,3 +1,33 @@
 
 let userSelect = document.querySelector('#date');
 userSelect.max = new Date().toISOString().split("T")[0];
+const calculateAgeBtn = document.querySelector('.js-calculate-age-btn')
+    .addEventListener('click', calculateAge);
+
+function calculateAge() {
+    let birth = new Date(userSelect.value);
+
+    let birthDay = birth.getDate();
+    let birthMonth = birth.getMonth() + 1;
+    let birthYear = birth.getYear();
+
+    let present  = new Date();
+
+    let presentDay = present.getDate();
+    let presentMonth = present.getMonth() + 1;
+    let presentYear = present.getYear();
+
+    let getDay, getMonth, getYear;
+    getDay = presentDay - birthDay;
+    getMonth = presentMonth - birthMonth;
+    getYear = presentYear - birthYear;
+
+    if(presentMonth >= birthMonth) getMonth = presentMonth - birthMonth;
+    else {
+        getYear--;
+        getMonth = 12 + presentMonth - birthMonth;
+    }
+
+    if(presentDay >= birthDay) getDay = presentDay - birthDay;
+    
+}
