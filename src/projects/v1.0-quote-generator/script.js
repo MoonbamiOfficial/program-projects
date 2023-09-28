8618c8c57d8bc665266d7bd452257df8f4fa1cd9
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-09-28 22:32:02 +0800

    feat: add function and selectors; add getQuote async function and add class name to element tags

--- src/projects/v1.0-quote-generator/script.js
+++ src/projects/v1.0-quote-generator/script.js
@@ -1,1 +1,15 @@
 
+const apiUrl = "https://api.quotable.io/random";
+const quoteDisplay = document.querySelector('.js-quote-display');
+const authorDisplay = document.querySelector('.js-author-display');
+const newQuoteBtn = document.querySelector('.js-new-quote-btn')
+    .addEventListener('click', () => getQuote(apiUrl));
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    quoteDisplay.textContent = data.content;
+    authorDisplay.textContent = data.author;
+}
+
+getQuote(apiUrl);
