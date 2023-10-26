e0163407d7b6fc39f03f9b21af74cd1e19afad28
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-27 00:26:30 +0800

    feat: add scss to tts

--- src/pages/projects/text-to-speech/TextToSpeech.jsx
+++ src/pages/projects/text-to-speech/TextToSpeech.jsx
@@ -1,13 +1,16 @@
 
 import React from 'react'
 import { useEffect } from 'react';
+import './style.scss';
 
 const TextToSpeech = () => {
   useEffect(() => {
     document.title = "Text to Speech";
   })
   return (
-    <section className="text-to-speech-section">this is text to speech</section>
+    <section className="text-to-speech-section">
+
+    </section>
   )
 }
 
