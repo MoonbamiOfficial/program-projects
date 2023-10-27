4ea260d5cf51129d02e45f9dc4a72e46a7c8b1fc
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-27 20:53:26 +0800

    feat: add speech synthesis utterance

--- src/pages/projects/text-to-speech/TextToSpeech.jsx
+++ src/pages/projects/text-to-speech/TextToSpeech.jsx
@@ -7,14 +7,30 @@ const TextToSpeech = () => {
   useEffect(() => {
     document.title = "Text to Speech";
   })
+
+  let speech = new SpeechSynthesisUtterance();
+  let voices = [];
+  let voiceSelect = document.querySelector('select');
+
+  
+  window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[2];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+  }
+
   return (
     <section className="text-to-speech-section">
       <h1>Text to Speech</h1>
       <textarea name="" id="input-area" className="js-input-area" placeholder="Aa"></textarea>
       <div className="row">
-        <select name="" id="voices" className="js-voices"></select>
-        <button className="js-listen-btn listen-btn">
-          <i class="fa-solid fa-play"></i>
+        <select id="voiceSelect" onChange={() => speech.voice = voices[voiceSelect.value]}></select>
+        <button className="js-listen-btn listen-btn" onClick={() => {
+          speech.text = document.querySelector('.js-input-area').value;
+          window.speechSynthesis.speak(speech);
+        }}>
+          <i className="fa-solid fa-play"></i>
           Listen
         </button>
       </div>
