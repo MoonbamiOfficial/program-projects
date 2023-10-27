964fe591591df115fe2fc088a59c10ec15735a7f
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-27 20:55:37 +0800

    feat: add new page introduction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,9 @@ const Home = () => {
   
   return (
     <section className="hero">
-      <h2>JavaScript Projects of Moonbami</h2>
+      <h2>
+        A simple <span className="js-span">JavaScript</span> projects using <span className="vite-span">Vite</span> for learning purposes.
+      </h2>
       <img src={ Building } alt="High-rise Buildings During Nighttime" />
     </section>
   );
