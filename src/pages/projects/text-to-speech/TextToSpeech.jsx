a320ae9e76ad9ab368bb348f6245bb7a39b29921
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-27 23:15:17 +0800

    fix: voice select bug; voice select is not showing

--- src/pages/projects/text-to-speech/TextToSpeech.jsx
+++ src/pages/projects/text-to-speech/TextToSpeech.jsx
@@ -4,28 +4,31 @@ import { useEffect } from 'react';
 import './style.scss';
 
 const TextToSpeech = () => {
-  useEffect(() => {
-    document.title = "Text to Speech";
-  })
-
   let speech = new SpeechSynthesisUtterance();
   let voices = [];
-  let voiceSelect = document.querySelector('select');
-
   
-  window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[2];
-
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-  }
+  useEffect(() => {
+    document.title = "Text to Speech";
+    let voiceSelect = document.querySelector('#voiceSelect');
+    
+    window.speechSynthesis.onvoiceschanged = function() {
+      voices = window.speechSynthesis.getVoices();
+      speech.voice = voices[0];
+      
+      voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+      
+    }
+    voiceSelect.addEventListener('change', () => {
+      speech.voice = voices[voiceSelect.value];
+    })
+  }, [])
 
   return (
     <section className="text-to-speech-section">
       <h1>Text to Speech</h1>
       <textarea name="" id="input-area" className="js-input-area" placeholder="Aa"></textarea>
       <div className="row">
-        <select id="voiceSelect" onChange={() => speech.voice = voices[voiceSelect.value]}></select>
+        <select id="voiceSelect"></select>
         <button className="js-listen-btn listen-btn" onClick={() => {
           speech.text = document.querySelector('.js-input-area').value;
           window.speechSynthesis.speak(speech);
