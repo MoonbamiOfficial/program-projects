caf7526563ec83f5159b1452f8c4a898cd87494a
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-28 23:43:24 +0800

    feat: add draggable elements

--- src/pages/projects/drag-and-drop/DragAndDrop.jsx
+++ src/pages/projects/drag-and-drop/DragAndDrop.jsx
@@ -10,8 +10,25 @@ const DragAndDrop = () => {
   
   return (
     <section className="drag-and-drop-section">
-      
-
+      <div id="left-box" className="box"></div>
+      <div id="right-box" className="box">
+        <div className="item" draggable="true">
+          <i className="fa-solid fa-grip-vertical"></i>
+          Item One
+        </div>
+        <div className="item" draggable="true">
+          <i className="fa-solid fa-grip-vertical"></i>
+          Item Two
+        </div>
+        <div className="item" draggable="true">
+          <i className="fa-solid fa-grip-vertical"></i>
+          Item Three
+        </div>
+        <div className="item" draggable="true">
+          <i className="fa-solid fa-grip-vertical"></i>
+          Item Four
+        </div>
+      </div>
     </section>
   )
 }
