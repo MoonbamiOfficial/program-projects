d24048419685511118c9472b321734937212306e
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-28 23:20:24 +0800

    feat: add useEffect

--- src/pages/projects/drag-and-drop/DragAndDrop.jsx
+++ src/pages/projects/drag-and-drop/DragAndDrop.jsx
@@ -1,7 +1,12 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const DragAndDrop = () => {
+
+  useEffect(() => {
+    document.title = "Drag & Drop";
+  }, [])
+  
   return (
     <section className="drag-and-drop-section"></section>
   )
